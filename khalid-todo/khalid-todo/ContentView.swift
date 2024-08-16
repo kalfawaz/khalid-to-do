482e3bcf0d0e0a90700ee4269166a0a7ae5d4f07
kalfawaz <khaled.alfawaz1212@gmail.com>
2024-08-16 18:45:53 +0300

    set project

---
+++ khalid-todo/khalid-todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  khalid-todo
+//
+//  Created by  khalid on 12/02/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ khalid-todo/khalid-todo/khalid_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  khalid_todoApp.swift
+//  khalid-todo
+//
+//  Created by  khalid on 12/02/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct khalid_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
